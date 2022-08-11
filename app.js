528643c897fc8e36f6bf9acaa3aa24180c9aef5d
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-08-10 20:50:48 -0700

    express hooked up

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const PORT = 4000;
 
-// test
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.json("hi");
+});
+
+app.get("/", (req, res, next) => {});
+
+app.listen(4000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
