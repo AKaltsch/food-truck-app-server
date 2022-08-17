538811a666c9509ffce490cd5564347a08ef098a
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-08-17 16:34:44 -0700

    MongoDB is connected!!

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const csrf = require("csurf");
+
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 require("dotenv").config();
 
+const MONGODB_URI = process.env.MONGODB_KEY;
+
 const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.get("/", (req, res, next) => {
   res.json("hi");
@@ -15,6 +25,11 @@ app.get("/", (req, res, next) => {
 
 app.get("/", (req, res, next) => {});
 
-app.listen(4000, () => {
-  console.log(`Server is running on port 4000`);
-});
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(4000, () => {
+      console.log(`Server is running on port 4000`);
+    });
+  })
+  .catch((err) => console.log(err));
