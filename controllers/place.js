a446d07203e73253d927f3c9affd878b64261921
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-09-16 15:32:08 -0700

    postPlace model/controller/route complete

---
+++ controllers/place.js
@@ -0,0 +1,21 @@
+const Place = require("../models/place");
+
+exports.postPlace = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const artistName = req.body.artistName;
+
+  const place = new Place({
+    title: title,
+    imageUrl: imageUrl,
+    artistName: artistName,
+  });
+
+  place
+    .save()
+    .then((result) => {
+      console.log("place created!!!");
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
